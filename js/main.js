2bf1757f37096079183ecb31021f88440818e9e8
JoaoChrisostomo <maturandomassas@gmail.com>
2022-05-17 14:39:15 -0300

    criando o carousel da home

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         },
+//     }
+// })
+
+ $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoPlaySpeed: 1000,
+    smartSpeed: 1000,
+    autoplayHoverPause: true,
+});
+
+
+
+
