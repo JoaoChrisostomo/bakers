73907432acc198b8c8f03aa9120e189213e525c0
JoaoChrisostomo <maturandomassas@gmail.com>
2022-05-18 01:39:22 -0300

    refazendo a parte de produtos adicionei um carrousel diferente

--- js/main.js
+++ js/main.js
@@ -31,3 +31,21 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
 });
+
+var swiper1 = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
